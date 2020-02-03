e8c38f05eae66a7ea3c492390bf9fa243380d381
Taylor Benjamin <benjamin.190@buckeyemail.osu.edu>
2020-02-03 15:03:09 -0500

    Player Factory edit

--- States/StillLinkState.cs
+++ States/StillLinkState.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -55,7 +56,7 @@ namespace Sprint0
 
         public void Update()
         {
-            //Nothing to do
+            link.sprite.Position = new Point(link.xPos, link.yPos);
         }
     }
 }
