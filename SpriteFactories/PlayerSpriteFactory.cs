614b5a5eb3ff895144ca7963d1f0716bc15425cd
Taylor Benjamin <benjamin.190@buckeyemail.osu.edu>
2020-02-03 20:49:57 -0500

    Contintued to work on sprite factory, still having issues with down and up attacking sprites

--- SpriteFactories/PlayerSpriteFactory.cs
+++ SpriteFactories/PlayerSpriteFactory.cs
@@ -10,7 +10,6 @@ namespace Sprint0
     class PlayerSpriteFactory
     {
         private Texture2D linkSpriteSheet;
-        private Texture2D upStillLink;
         private static PlayerSpriteFactory instance = new PlayerSpriteFactory();
 
         private PlayerSpriteFactory() { }
@@ -26,7 +25,6 @@ namespace Sprint0
         public void LoadTextures(ContentManager content)
         {
             linkSpriteSheet = content.Load<Texture2D>("link");
-            upStillLink = content.Load<Texture2D>("StillLinkUp");
         }
 
         public ISprite CreateLeftWalkingLinkSprite()
@@ -61,7 +59,7 @@ namespace Sprint0
 
         public ISprite CreateUpAttackingLinkSprite()
         {
-            return new AnimatedSprite(linkSpriteSheet, new Rectangle(60, 60, 18, 26), 2, false);
+            return new AnimatedSprite(linkSpriteSheet, new Rectangle(60, 50, 18, 31), 2, false);
         }
 
         public ISprite CreateDownAttackingLinkSprite()
@@ -71,46 +69,22 @@ namespace Sprint0
 
         public ISprite CreateLeftStillLinkSprite()
         {
-            //Crop spritesheet to contain only desired sprites
-            //Help from https://gamedev.stackexchange.com/questions/35358/create-a-texture2d-from-larger-image
-            Rectangle sourceRectangle = new Rectangle(30, 0, 15, 16);
-
-            Texture2D leftStillLink = new Texture2D(linkSpriteSheet.GraphicsDevice, sourceRectangle.Width, sourceRectangle.Height);
-            Color[] data = new Color[sourceRectangle.Width * sourceRectangle.Height];
-            linkSpriteSheet.GetData(0, sourceRectangle, data, 0, data.Length);
-            leftStillLink.SetData(data);
-            return new Sprite(leftStillLink, new Rectangle(0, 0, 15, 16));
+            return new Sprite(linkSpriteSheet, new Rectangle(30, 0, 16, 17));
         }
 
         public ISprite CreateRightStillLinkSprite()
         {
-            //Crop spritesheet to contain only desired sprites
-            //Help from https://gamedev.stackexchange.com/questions/35358/create-a-texture2d-from-larger-image
-            Rectangle sourceRectangle = new Rectangle(90, 30, 15, 16);
-
-            Texture2D rightStillLink = new Texture2D(linkSpriteSheet.GraphicsDevice, sourceRectangle.Width, sourceRectangle.Height);
-            Color[] data = new Color[sourceRectangle.Width * sourceRectangle.Height];
-            linkSpriteSheet.GetData(0, sourceRectangle, data, 0, data.Length);
-            rightStillLink.SetData(data);
-            return new Sprite(rightStillLink, new Rectangle(0, 0, 15, 16));
+            return new Sprite(linkSpriteSheet, new Rectangle(90, 0, 15, 17));
         }
 
         public ISprite CreateUpStillLinkSprite()
         {   
-            return new Sprite(upStillLink, new Rectangle(0, 0, upStillLink.Width, upStillLink.Height));
+            return new Sprite(linkSpriteSheet, new Rectangle(62, 0, 12, 17));
         }
 
         public ISprite CreateDownStillLinkSprite()
         {
-            //Crop spritesheet to contain only desired sprites
-            //Help from https://gamedev.stackexchange.com/questions/35358/create-a-texture2d-from-larger-image
-            Rectangle sourceRectangle = new Rectangle(30, 0, 14, 16);
-
-            Texture2D downStillLink = new Texture2D(linkSpriteSheet.GraphicsDevice, sourceRectangle.Width, sourceRectangle.Height);
-            Color[] data = new Color[sourceRectangle.Width * sourceRectangle.Height];
-            linkSpriteSheet.GetData(0, sourceRectangle, data, 0, data.Length);
-            downStillLink.SetData(data);
-            return new Sprite(downStillLink, new Rectangle(0, 0, 14, 16));
+            return new Sprite(linkSpriteSheet, new Rectangle(0, 0, 15, 17));
         }
     }
 }
