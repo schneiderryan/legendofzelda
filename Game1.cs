428cf763f034c59a0794aa18c47741dae0dde8e1
Emily <57276954+EmilyBoxedIn@users.noreply.github.com>
2020-02-10 14:02:17 -0500

    MergedTaylorBranchToMain

--- Game1.cs
+++ Game1.cs
@@ -7,6 +7,7 @@ namespace Sprint0
 {
     public class Game1 : Game
     {
+<<<<<<< HEAD
         public GraphicsDeviceManager graphics;
         public SpriteBatch spriteBatch;
         IController keyboard;
@@ -16,6 +17,14 @@ namespace Sprint0
         public Texture2D SpriteSheet { get; set; }
         public ISprite Sprite { get; set; }
         IEnemy goriya;
+=======
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public Texture2D SpriteSheet { get; set; }
+        public ISprite Sprite { get; set; }
+        IPlayer link;
+>>>>>>> Taylor
 
         public Game1()
         {
@@ -33,13 +42,22 @@ namespace Sprint0
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+<<<<<<< HEAD
             EnemySpriteFactory.Instance.LoadTextures(Content);
             this.goriya = new Goriya();
+=======
+            PlayerSpriteFactory.Instance.LoadTextures(Content);
+            this.link = new RedLink();
+>>>>>>> Taylor
         }
 
         protected override void Update(GameTime gameTime)
         {
+<<<<<<< HEAD
             goriya.Update();
+=======
+            link.Update();
+>>>>>>> Taylor
             base.Update(gameTime);
         }
 
@@ -47,20 +65,61 @@ namespace Sprint0
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+<<<<<<< HEAD
             // This gets rid of blurry scaling
             // https://gamedev.stackexchange.com/a/6822
             // spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+=======
+            //// This gets rid of blurry scaling
+            //// https://gamedev.stackexchange.com/a/6822
+            //spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+>>>>>>> Taylor
 
             //background.Draw(spriteBatch);
             //Sprite.Draw(spriteBatch);
             //spriteBatch.End();
+<<<<<<< HEAD
             spriteBatch.Begin();
             goriya.Draw(spriteBatch);
             spriteBatch.End();
+=======
+>>>>>>> Taylor
 
+            spriteBatch.Begin();
+            link.Draw(spriteBatch);
+            spriteBatch.End();
             base.Draw(gameTime);
         }
 
+<<<<<<< HEAD
         
+=======
+        //private Dictionary<Keys, ICommand> GenerateKeyBinds()
+        //{
+        //    Dictionary<Keys, ICommand> keyBinds = new Dictionary<Keys, ICommand>();
+
+        //    ICommand cmd = new DancingSpriteCommand(this);
+        //    keyBinds.Add(Keys.NumPad4, cmd);
+        //    keyBinds.Add(Keys.D4, cmd);
+
+        //    cmd = new MoveSpriteCommand(this);
+        //    keyBinds.Add(Keys.NumPad3, cmd);
+        //    keyBinds.Add(Keys.D3, cmd);
+
+        //    cmd = new AnimatedSpriteCommand(this);
+        //    keyBinds.Add(Keys.NumPad2, cmd);
+        //    keyBinds.Add(Keys.D2, cmd);
+
+        //    cmd = new StaticSpriteCommand(this);
+        //    keyBinds.Add(Keys.NumPad1, cmd);
+        //    keyBinds.Add(Keys.D1, cmd);
+
+        //    cmd = new QuitCommand(this);
+        //    keyBinds.Add(Keys.NumPad0, cmd);
+        //    keyBinds.Add(Keys.D0, cmd);
+
+        //    return keyBinds;
+        //}
+>>>>>>> Taylor
     }
 }
