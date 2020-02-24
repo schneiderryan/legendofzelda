24e693c1c4b200c5eba15119cfb162ea24dc36dd
schneiderryan <schneiderryan2017@gmail.com>
2020-02-24 14:30:50 -0500

    rearraged the command folder, and added a working boomerang

---
+++ Commands/Collision/PlayerItemCollisionCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegendOfZelda
+{
+    class PlayerItemCollisionCommand : ICommand
+    {
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Items/Boomerang.cs
+++ Items/Boomerang.cs
@@ -12,9 +12,6 @@ namespace LegendOfZelda
 
         public override void Use(IPlayer player)
         {
-            IProjectile proj = new BoomerangProjectile(player.direction,
-                player.xPos, player.yPos);
-            player.UseProjectile(proj);
         }
 
        
