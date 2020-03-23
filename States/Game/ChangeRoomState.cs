8c14f502bc9060451b08e4f2a7ef2bd982124ff9
Taylor Benjamin <benjamin.190@buckeyemail.osu.edu>
2020-03-23 16:34:26 -0400

    start game state stuff

---
+++ States/Game/ChangeRoomState.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegendOfZelda
+{
+    class ChangeRoomState : IGameState
+    {
+        private LegendOfZelda game;
+        public ChangeRoomState(LegendOfZelda game)
+        {
+            this.game = game;
+        }
+
+        public void PlayGame()
+        {
+            game.state = new PlayState(game);
+        }
+
+        public void PauseGame()
+        {
+            game.state = new PauseState(game);
+        }
+
+        public void ResumeGame()
+        {
+            game.state = new PlayState(game);
+        }
+
+        public void ChangeRoom()
+        {
+            //Nothing to do
+        }
+
+        public void WinGame()
+        {
+            //Nothing to do
+        }
+
+        public void LoseGame()
+        {
+            //Nothing to do
+        }
+
+        public void SelectItem()
+        {
+            //Nothing to do
+        }
+
+        public void Update()
+        {
+            //Figure out later
+        }
+    }
+}
---
+++ States/Game/PlayState.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegendOfZelda
+{
+    class PlayState : IGameState
+    {
+        private LegendOfZelda game;
+        public PlayState(LegendOfZelda game)
+        {
+            this.game = game;
+        }
+
+        public void PlayGame()
+        {
+            //Nothing to do
+        }
+
+        public void PauseGame()
+        {
+            game.state = new PauseState(game);
+        }
+
+        public void ResumeGame()
+        {
+            //Nothing to do
+        }
+
+        public void ChangeRoom()
+        {
+            game.state = new ChangeRoomState(game);
+        }
+
+        public void WinGame()
+        {
+            game.state = new WinState(game);
+        }
+
+        public void LoseGame()
+        {
+            game.state = new LoseState(game);
+        }
+
+        public void SelectItem()
+        {
+            game.state = new SelectItemState(game);
+        }
+
+        public void Update()
+        {
+            //Figure out later
+        }
+    }
+}
