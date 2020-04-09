3ced3445519a3f161314c3e36e6ffcebc0886d27
ImprobabilityCast <segmentationfault3@outlook.com>
2020-04-09 01:59:12 -0400

    fix formatting

--- Collision/PlayerDoorCollision.cs
+++ Collision/PlayerDoorCollision.cs
@@ -8,14 +8,11 @@ namespace LegendOfZelda
 {
     class PlayerDoorCollision : ICollision
     {
-
         LegendOfZelda game;
-
-
-        private IPlayer player;
-        private Rectangle collision;
-        private IDictionary<string, IDoor> doors;
-        private KeyValuePair<string, IDoor> door;
+        IPlayer player;
+        Rectangle collision;
+        IDictionary<string, IDoor> doors;
+        KeyValuePair<string, IDoor> door;
 
         static ICommand cmdLeft;
         static ICommand cmdRight;
@@ -33,10 +30,7 @@ namespace LegendOfZelda
             this.door = door;
         }
 
-       
-
         public void Handle()
-
         {
             if (door.Value is TopOpen || door.Value is BottomOpen || door.Value is LeftOpen || door.Value is RightOpen)
             {
@@ -97,38 +91,35 @@ namespace LegendOfZelda
             }
         }
 
-            public void HandleEdge(IPlayer player, IDoor door, Rectangle collision, LegendOfZelda game)
-            {
-
+        public void HandleEdge(IPlayer player, IDoor door, Rectangle collision, LegendOfZelda game)
+        {
             cmdRight = new SwapRoomCommand(game, "next");
             cmdLeft = new SwapRoomCommand(game, "previous");
             cmdUp = new SwapRoomCommand(game, "up");
             cmdDown = new SwapRoomCommand(game, "down");
+
             //change rooms based on door collision
             if (door is TopOpen)
-                {
-                    cmdUp.Execute();
-                    player.Y = 280;
-                }
-                if (door is BottomOpen)
-                {
-                    cmdDown.Execute();
-                    player.Y = 60;
-                }
-                if (door is LeftOpen)
-                {
-                    cmdLeft.Execute();
-                    player.X = 417;
-                }
-                if (door is RightOpen)
-                {
-                    cmdRight.Execute();
-                    player.X = 60;
+            {
+                cmdUp.Execute();
+                player.Y = 280;
+            }
+            if (door is BottomOpen)
+            {
+                cmdDown.Execute();
+                player.Y = 60;
+            }
+            if (door is LeftOpen)
+            {
+                cmdLeft.Execute();
+                player.X = 417;
+            }
+            if (door is RightOpen)
+            {
+                cmdRight.Execute();
+                player.X = 60;
 
-                }
             }
         }
     }
-
-    
-
+}
