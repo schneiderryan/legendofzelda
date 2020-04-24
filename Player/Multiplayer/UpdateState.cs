569343b129e9aed81ab0be66ac2b35fbb27fd678
The Magical Unicorn <smith.11725@buckeyemail.osu.edu>
2020-04-24 18:09:50 -0400

    mulitplayer wp :(

---
+++ Player/Multiplayer/UpdateState.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+
+namespace LegendOfZelda
+{
+    class UpdateState
+    {
+        public bool Ready { get; set; }
+        public UdpClient Udp { get; private set; }
+        public IPEndPoint Endpoint { get; private set; }
+
+        public UpdateState(UdpClient udp, IPEndPoint end)
+        {
+            Ready = false;
+            Udp = udp;
+            Endpoint = end;
+        }
+    }
+}
